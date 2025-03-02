3991fa88490995c097d22731a4936831ad462b3c
Shannto <77185420+Shannto@users.noreply.github.com>
2025-03-03 01:21:26 +0600

    m1

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/uc?export=download&id=1bZGtF5uJMeWPuhakfmixkDMvm8DQzeVf"
+              href="https://drive.google.com/uc?export=download&id=1DyBJBVzzcVq0j0GXPBrW8Sb9OtIWmLDp"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
